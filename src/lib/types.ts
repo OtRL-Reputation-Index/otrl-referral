c15200efb3184b8f8b2888b803b2e9cb9ddad9b6
KumailB <63493921+KumailB@users.noreply.github.com>
2022-03-13 20:10:36 -0500

    refactor: added all required types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,16 +1,71 @@
 export type Employee = {
   id: Number;
+  pk: string;
   firstName: string;
   middleName: string | undefined;
   lastName: string;
   phoneNum: string;
-  pk: string;
-  numReferrals: Number;
+  email: string;
   rui: Number;
+  numReferrals: Number;
+};
+
+export type Employer = {
+  id: Number;
+  pk: string;
+  companyName: string;
+  firstName: string;
+  middleName: string | undefined;
+  lastName: string;
+  phoneNum: string;
+  email: string;
+};
+
+export type Referral = {
+  id: Number;
+  employeePk: string;
+  employerPk: string;
+  message: string;
+  signature: string;
+  sWorkTime: Number;
+  sPressure: Number;
+  sEthic: Number;
+  sComplaints: Number;
+  sRespect: Number;
+  sReliable: Number;
+  sTaskTime: Number;
+  sTaskComplete: Number;
+  sTaskCommunicate: Number;
+  sTaskExperience: Number;
+  submittedAt: Date;
 };
 
 export interface EmployeeUpdate {
   id: Number;
   newRui: Number;
   newNumReferrals: Number;
+  lastUpdated: Date;
+}
+
+export interface EmployeeGet {
+  employeePk: Number;
+}
+
+export interface EmployerGet {
+  employerPk: Number;
+}
+
+export interface MessageGet {
+  message: string;
+}
+
+export interface SignatureVerify {
+  pk: string;
+  message: string;
+  signature: string;
+}
+
+export interface Submit {
+  referral: Referral;
+  employeeUpdate: EmployeeUpdate;
 }
