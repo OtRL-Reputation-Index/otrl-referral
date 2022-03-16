590980c7951fd4e7e478f0148bbffd2d84c94201
KumailB <63493921+KumailB@users.noreply.github.com>
2022-03-16 07:22:42 -0500

    Middletier ws (#4)
    * refactor: added all required types
    
    * refactor: added referral base code
    
    * fix: types of employee and employer pk
    
    * refactor: updated interfaces
    
    * post referral object to dynamoDB
    
    * partial submit update and weights json
    
    * chore: cleanup
    
    Co-authored-by: was170001 <64547588+was170001@users.noreply.github.com>

--- src/pages/api/db/referral.ts
+++ src/pages/api/db/referral.ts
@@ -1,8 +1,38 @@
 import { Referral } from "@/lib/types";
+import AWS from "@/pages/api/db/aws";
 
-const postEmployee = async (referral: Referral): Promise<boolean> => {
-  console.log(referral);
+const postReferral = async (referral: Referral): Promise<boolean> => {
+  const docClient = new AWS.DynamoDB.DocumentClient();
+
+  const table = "Referral";
+  const params = {
+    TableName: table,
+    Key: {
+      id: referral.id, // partition key
+    },
+    Item: {
+      message: referral.message,
+      s_complaints: referral.sComplaints,
+      s_ethic: referral.sEthic,
+      s_pressure: referral.sPressure,
+      s_reliable: referral.sReliable,
+      s_respect: referral.sRespect,
+      s_task_communicate: referral.sTaskCommunicate,
+      s_task_complete: referral.sTaskComplete,
+      s_task_experience: referral.sTaskExperience,
+      s_task_time: referral.sTaskTime,
+      s_work_time: referral.sWorkTime,
+      signature: referral.signature,
+    },
+  };
+
+  try {
+    await docClient.put(params).promise();
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
   return true;
 };
 
-export { postEmployee };
+export { postReferral };
